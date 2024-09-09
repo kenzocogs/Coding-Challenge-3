ce10d18e0143df2645182fbd1417004fb54eedc2
Kenzo Cogswell <kcogswell@usf.edu>
2024-09-08 23:07:17 -0400

    Create determinePerformanceRating function

--- perfAnalyzer.js
+++ perfAnalyzer.js
@@ -10,3 +10,25 @@ function calculateAverageSales (sales) {
 console.log ((3000+4000+3500)/3) // Output: 3500
 
 
+// Task 2: Creating a function to determine performance rating
+
+let AverageSales = 3500
+function determinePerformanceRating (AverageSales) {
+    if (AverageSales > 10000) {
+        console.log(`Performance Rating: Excellent.`);
+    }
+    else if (7000 <= AverageSales <= 10000)  {
+        console.log(`Performance Rating: Good`);
+    }
+    else if (4000 <= AverageSales <= 7000) {
+        console.log(`Performance Rating: Satisfactory `);
+    }
+    else (AverageSales < 4000) 
+        console.log(`Performance Rating: Needs Improvement`);
+
+        return 
+}
+
+ // Output: "Performance Rating: Needs Improvement"
+    
+ 
