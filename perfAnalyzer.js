57bc797eb782d621cc32f16e1134155e9ba958a3
Kenzo Cogswell <kcogswell@usf.edu>
2024-09-08 22:18:33 -0400

    Implement calculateAverageSales function

---
+++ perfAnalyzer.js
@@ -0,0 +1,12 @@
+// Task 1: Creating a function to calculate average sales
+
+const sales = [3000, 4000, 3500];
+sales.reduce(calculateAverageSales,0);
+
+function calculateAverageSales (sales) {
+    return 
+}
+
+console.log ((3000+4000+3500)/3) // Output: 3500
+
+
